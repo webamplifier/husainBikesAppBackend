65db670e32b883399dd8b82459629d28d92402d1
alihussainkabri <alihussainkabri52@gmail.com>
2021-12-15 19:08:38 +0530

    changes

--- controllers/ServiceController.js
+++ controllers/ServiceController.js
@@ -8,7 +8,7 @@ router.list = async (req, res) => {
     let message = 'Oops something went wrong!';
     let service_list = [];
 
-    await knex('services').where("user_id",req.user_data.id).orderBy("id", "desc").then(response => {
+    await knex('services').where("user_id", req.user_data.id).orderBy("id", "desc").then(response => {
         if (response) {
             status = 200;
             message = 'Service list has been fetched successfully!';
@@ -25,7 +25,51 @@ router.create = async (req, res) => {
     let message = 'Oops something went wrong!';
     let inputs = req.body;
 
-    
+    let user = {};
+
+    let priority_user_0 = [];
+    let priority_user_1 = [];
+    let priority_user_2 = [];
+    let priority_user_3 = [];
+    let priority_user_4 = [];
+    let priority_user_5 = [];
+
+    await knex('users').where('active', 1).then(async response => {
+        if (response.length > 0) {
+            for (let i = 0; i < response.length; i++) {
+
+                await knex('services').where('assign_id', response[i].id).where('status', 2).then(async response_service => {
+                    if (response_service.length > 0) {
+
+                        if (response_service.length == 1) {
+                            priority_user_1 = [{ id: response[i].id, name: response[i].name }, ...priority_user_1]
+                        }
+                        if (response_service.length == 2) {
+                            priority_user_2 = [{ id: response[i].id, name: response[i].name }, ...priority_user_2]
+                        }
+                        if (response_service.length == 3) {
+                            priority_user_3 = [{ id: response[i].id, name: response[i].name }, ...priority_user_3]
+                        }
+                        if (response_service.length == 4) {
+                            priority_user_4 = [{ id: response[i].id, name: response[i].name }, ...priority_user_4]
+                        }
+                        if (response_service.length == 5) {
+                            priority_user_5 = [{ id: response[i].id, name: response[i].name }, ...priority_user_5]
+                        }
+
+                    } else {
+                        priority_user_0 = [{ id: response[i].id, name: response[i].name }, ...priority_user_0]
+                    }
+                })
+
+            }
+        } else {
+            return res.json({
+                status : 500,
+                message : "No mechanic is available now"
+            })
+        }
+    });
 
     let create_obj = {
         uuid: await HELPERS.getKnexUuid(knex),
@@ -38,27 +82,102 @@ router.create = async (req, res) => {
         demand_dateTime: await HELPERS.dateTime(),
     }
 
-    await knex('services').insert(create_obj).then(response => {
-        if (response) {
-            status = 200;
-            message = 'Service has been created successfully!';
-        }
-    }).catch(err => console.log(err))
+    if (priority_user_0.length > 0) {
+        create_obj['assign_id'] = priority_user_0[0].id;
+        create_obj['assign_name'] = priority_user_0[0].name;
+        create_obj['assign_dateTime'] = await HELPERS.dateTime();
+        create_obj['status'] = 2
+        await knex('services').insert(create_obj).then(response => {
+            if (response) {
+                status = 200;
+                message = 'Service has been created successfully!';
+            }
+        }).catch(err => console.log(err))
 
-    return res.json({ status, message })
+        return res.json({ status, message })
+    } elseif(priority_user_1.length > 0){
+        create_obj['assign_id'] = priority_user_1[0].id;
+        create_obj['assign_name'] = priority_user_1[0].name;
+        create_obj['assign_dateTime'] = await HELPERS.dateTime();
+        create_obj['status'] = 2
+        await knex('services').insert(create_obj).then(response => {
+            if (response) {
+                status = 200;
+                message = 'Service has been created successfully!';
+            }
+        }).catch(err => console.log(err))
+
+        return res.json({ status, message })
+
+    } elseif(priority_user_2.length > 0){
+        create_obj['assign_id'] = priority_user_2[0].id;
+        create_obj['assign_name'] = priority_user_2[0].name;
+        create_obj['assign_dateTime'] = await HELPERS.dateTime();
+        create_obj['status'] = 2
+        await knex('services').insert(create_obj).then(response => {
+            if (response) {
+                status = 200;
+                message = 'Service has been created successfully!';
+            }
+        }).catch(err => console.log(err))
+
+        return res.json({ status, message })
+    } elseif(priority_user_3.length > 0){
+        create_obj['assign_id'] = priority_user_3[0].id;
+        create_obj['assign_name'] = priority_user_3[0].name;
+        create_obj['assign_dateTime'] = await HELPERS.dateTime();
+        create_obj['status'] = 2
+        await knex('services').insert(create_obj).then(response => {
+            if (response) {
+                status = 200;
+                message = 'Service has been created successfully!';
+            }
+        }).catch(err => console.log(err))
+
+        return res.json({ status, message })
+    } elseif(priority_user_4.length > 0){
+        create_obj['assign_id'] = priority_user_4[0].id;
+        create_obj['assign_name'] = priority_user_4[0].name;
+        create_obj['assign_dateTime'] = await HELPERS.dateTime();
+        create_obj['status'] = 2
+        await knex('services').insert(create_obj).then(response => {
+            if (response) {
+                status = 200;
+                message = 'Service has been created successfully!';
+            }
+        }).catch(err => console.log(err))
+
+        return res.json({ status, message })
+    } elseif(priority_user_5.length > 0){
+        create_obj['assign_id'] = priority_user_5[0].id;
+        create_obj['assign_name'] = priority_user_5[0].name;
+        create_obj['assign_dateTime'] = await HELPERS.dateTime();
+        create_obj['status'] = 2
+        await knex('services').insert(create_obj).then(response => {
+            if (response) {
+                status = 200;
+                message = 'Service has been created successfully!';
+            }
+        }).catch(err => console.log(err))
+
+        return res.json({ status, message })
+    }
+
+
+
 }
 
 //this is to assign a service
-router.assign = async (req, res) =>{
+router.assign = async (req, res) => {
     let status = 500;
     let message = 'Oops something went wrong!';
     let { id } = req.params;
     let inputs = req.body;
-    
+
     let assign_obj = {
         assign_id: inputs.assign_id,
         assign_name: inputs.assign_name,
-        assign_dateTime : await HELPERS.dateTime(),
+        assign_dateTime: await HELPERS.dateTime(),
         status: 2
     }
 
@@ -73,13 +192,13 @@ router.assign = async (req, res) =>{
 }
 
 //this is to denote that assignee has reached the place
-router.reached = async (req, res) =>{
+router.reached = async (req, res) => {
     let status = 500;
     let message = 'Oops something went wrong!';
     let { id } = req.params;
-    
+
     let reached_obj = {
-        reached_dateTime : await HELPERS.dateTime(),
+        reached_dateTime: await HELPERS.dateTime(),
         status: 3
     }
 
@@ -94,22 +213,22 @@ router.reached = async (req, res) =>{
 }
 
 //this is to complete a service
-router.completed = async (req, res) =>{
+router.completed = async (req, res) => {
     let status = 500;
     let message = 'Oops something went wrong!';
     let inputs = req.body;
     let { id } = req.params;
 
     let complete_obj = {
-        complete_dateTime : await HELPERS.dateTime(),
-        remarks : inputs.remarks,
-        total_amount : inputs.total_amount,
+        complete_dateTime: await HELPERS.dateTime(),
+        remarks: inputs.remarks,
+        total_amount: inputs.total_amount,
         status: 4
     }
 
-    await knex('services').where('id', id).then( async response => {
-        
-        if (response[0].assign_id == req.user_data.id) {    
+    await knex('services').where('id', id).then(async response => {
+
+        if (response[0].assign_id == req.user_data.id) {
             await knex('services').where('id', id).update(complete_obj).then(response1 => {
                 if (response1) {
                     status = 200;
@@ -117,9 +236,9 @@ router.completed = async (req, res) =>{
                 }
             }).catch(err => console.log(err))
         }
-        else{
+        else {
             status = 400;
-            message="You cannot perform this action";
+            message = "You cannot perform this action";
         }
     }).catch(err => console.log(err))
 
@@ -132,12 +251,12 @@ router.delete = async (req, res) => {
     let message = 'Oops something went wrong!';
     let { id } = req.params;
 
-    await knex('services').where('id', id).then( async response => {
+    await knex('services').where('id', id).then(async response => {
         if (response[0].status == 3) {
             status = 400;
             message = 'Service cannot be deleted now';
         }
-        else{
+        else {
             await knex('services').where('id', id).del().then(response1 => {
                 if (response1) {
                     status = 200;
