f4b986fe7765dc075456ab8f1fe4205440578a85
alihussainkabri <alihussainkabri52@gmail.com>
2022-01-14 19:28:08 +0530

    changes

--- configs/MailConfig.js
+++ configs/MailConfig.js
@@ -3,9 +3,9 @@ exports.provider = "smtp"; // provider name : gmail, smtp, sendmail
 exports.hostName = "mail.husainbikes.com";
 exports.port = 587;
 exports.isSecure = false; // true for 465, false for other ports
-exports.username = "info@husainbikes.com";
-exports.password = "aliyahya856206";
-exports.fromEmail = "info@husainbikes.com";
+exports.username = "contact@husainbikes.com";
+exports.password = "EDm_OnqvIES~";
+exports.fromEmail = "contact@husainbikes.com";
 // exports.provider = "gmail"; // provider name : gmail, smtp, sendmail
 // // mail configuration for gmail or smtp
 // exports.hostName = "";
